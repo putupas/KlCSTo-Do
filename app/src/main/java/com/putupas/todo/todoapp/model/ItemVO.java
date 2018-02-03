5603175caa7196bb9e35585ced51693fff52cab5
Your Name <you@example.com>
2018-02-03 15:58:56 +0200

    first commit

---
+++ app/src/main/java/com/putupas/todo/todoapp/model/ItemVO.java
@@ -0,0 +1,11 @@
+package com.putupas.todo.todoapp.model;
+
+/**
+ * Created by Andrius on 2/3/2018.
+ */
+
+public class ItemVO {
+    public int id;
+    public int done;
+    public String title;
+}
